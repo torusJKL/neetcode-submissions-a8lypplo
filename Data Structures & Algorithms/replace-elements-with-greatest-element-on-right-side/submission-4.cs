1858c9c5bc6b7a764b0f7e7e05275440b78511ed
Gal Buki <jkl@torus.ch>
2026-04-15 20:47:07 +0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-4

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-4.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] ReplaceElements(int[] arr) {
+
+        int rightMax = -1;
+        int[] result = new int[arr.Length];
+
+        for (int i = arr.Length - 1; i >= 0; i--){
+            result[i] = rightMax;
+            rightMax = arr[i] > rightMax ? arr[i] : rightMax;
+        }
+
+        return result;
+    }
+}
