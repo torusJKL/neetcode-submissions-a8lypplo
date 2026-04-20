3c59dac555248a94bcdb9f6584ee2de699f3dba2
Gal Buki <jkl@torus.ch>
2026-04-20 18:25:42 +0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+
+        var start = 0;
+        var end = nums.Length - 1;
+
+        while(start <= end){
+            var middle = (end - start) / 2 + start;
+
+            if (nums[middle] == target){
+                return middle;
+            }
+
+            if (nums[middle] < target){
+                start = middle + 1;
+            }
+            else if (nums[middle] > target){
+                end = middle - 1;
+            }
+        }
+
+        return -1;
+        
+    }
+}
