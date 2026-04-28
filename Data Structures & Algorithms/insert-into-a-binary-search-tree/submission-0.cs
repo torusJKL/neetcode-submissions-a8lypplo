8c2a3208c14d01d15860bab7e49b2adfaf0c5abd
Gal Buki <jkl@torus.ch>
2026-04-28 15:32:48 +0300

    Add: insert-into-a-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-0.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InsertIntoBST(TreeNode root, int val) {
+        if (root == null){
+            return new TreeNode(val);
+        }
+
+        if (root.val > val){
+            root.left = InsertIntoBST(root.left, val);
+        }
+        
+        if (root.val < val){
+            root.right = InsertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
